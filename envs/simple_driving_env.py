0ee6423bae7efa6a7be02903b68c6ee2ee7c038e
Kharyal <chaitanyajee@gmail.com>
2021-12-14 02:49:56 +0530

    code cleanup

--- envs/simple_driving_env.py
+++ envs/simple_driving_env.py
@@ -17,13 +17,31 @@ class SimpleDrivingEnv(gym.Env):
         self.action_space = gym.spaces.box.Box(
             low=np.array([0, -.6], dtype=np.float32),
             high=np.array([1, .6], dtype=np.float32))
-        self.observation_space = gym.spaces.box.Box(
-            low=np.array([-10, -10, -np.pi, -np.pi, -np.pi, -5, -5], dtype=np.float32),
-            high=np.array([10, 10, np.pi, np.pi, np.pi, 5, 5], dtype=np.float32))
+
+        """
+            obs_space -- observation space for the environment
+                car_qpos     -- x and y co-ordinates of the car, orientation of the car (Euler angles/pi), x and y velocities of the car
+                segmentation -- A 3d occupancy map with Region occupied by the car along the first layer, that occupied by the obstacles along the second layer,
+                                and the goal position along the third layer (not present for the agent setting the goal)  
+        """
+        obs_space = {
+            'car_qpos':gym.spaces.box.Box(
+                    low=np.array([-np.Inf, -np.Inf, -1, -1, -1, -np.Inf, -np.Inf], dtype=np.float32),
+                    high=np.array([np.Inf,  np.Inf,  1,  1,  1,  np.Inf,  np.Inf], dtype=np.float32)
+                ),
+            
+            'segmentation': gym.spaces.box.Box(
+                    low = np.zeros((75,75,3)),
+                    high = np.zeros((75,75,3)),
+            )
+            
+        }
+
+        self.observation_space = gym.spaces.dict.Dict(obs_space)
         self.np_random, _ = gym.utils.seeding.np_random()
 
         self.client = p.connect(p.DIRECT)
-        # print("Done")
+
         # Reduce length of episodes for RL algorithms
         p.setTimeStep(1/30, self.client)
 
@@ -35,43 +53,15 @@ class SimpleDrivingEnv(gym.Env):
         self.render_rot_matrix = None
         self.max_steps = 250
         self.steps = 0
-        # self.numobstacles = 6
-        # self.pos_obstacles = np.array([[7,7,0.1],
-        #                     [-3,4,0.1],
-        #                     [5,-6,0.1],
-        #                     [-7,-5,0.1],
-        #                     [1,-2, 0.1],
-        #                     [0,12,0.1],
-        #                     [0,-12,0.1],
-        #                     [12,0,0.1],
-        #                     [-12,0,0.1]])
-        self.pos_obstacles = np.array([[7,7,0.1],
-                            [-3,4,0.1],
-                            [5,-6,0.1],
-                            [-7,-5,0.1],
-                            [1,-2, 0.1],
-                            [0,12,0.1],
-                            [0,-12,0.1],
-                            [12,0,0.1],
-                            [-12,0,0.1]])
 
-        wall_width = 11
-        self.obstacle_dims = np.array([[1,1,0.2],
-                            [1,1,0.2],
-                            [1,1,0.2],
-                            [1,1,0.2],
-                            [1,1,0.2],
-                            [wall_width,1,0.2],
-                            [wall_width,1,0.2],
-                            [1,wall_width,0.2],
-                            [1,wall_width,0.2]])
-        # boxHalfLength = 5
-        # boxHalfWidth = 5
-        # boxHalfHeight = 5
-
-        # self.colBoxId = p.createCollisionShape(p.GEOM_BOX,
-        #                           halfExtents=[boxHalfLength, boxHalfWidth, boxHalfHeight])
+        # initialize the obstacles and their dimentions
+        self.pos_obstacles = None
+        self.obstacle_dims = None
+        self.initialize_obstacles()
+        
         self.obstacle_mass = 1000
+        
+        # setting up the camera
         self.viewMatrix = p.computeViewMatrix(
                     cameraEyePosition=[0, 0, 25],
                     cameraTargetPosition=[0, 0, 0],
@@ -82,10 +72,42 @@ class SimpleDrivingEnv(gym.Env):
                     nearVal=0.1,
                     farVal=100.5)
 
-        # self.obstacles = [[self.colBoxId, [5,5,0.1]]]
-        # p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 1)
-        
-        # self.reset()
+
+    def initialize_obstacles(self):
+        # TODO: once in running condition, change the position initialization to np.zeros
+        """
+            Write your own initialization
+            pos_onstacles changes every time reset for alice is called
+        """
+        # TODO: once in running condition, change the position initialization to np.zeros
+        self.pos_obstacles = np.array([[7,7,0.1],
+                            [-3,4,0.1],
+                            [5,-6,0.1],
+                            [-7,-5,0.1],
+                            [1,-2, 0.1],
+
+                            # positions of outer walls (constant throughout: see reset_obstacle_positions)
+                            [0,12,0.1],
+                            [0,-12,0.1],
+                            [12,0,0.1],
+                            [-12,0,0.1]])
+
+        wall_width = 11
+        self.obstacle_dims = np.array([
+                            # obstacles
+                            [1,1,0.2],
+                            [1,1,0.2],
+                            [1,1,0.2],
+                            [1,1,0.2],
+                            [1,1,0.2],
+
+                            #outer walls
+                            [wall_width,1,0.2],
+                            [wall_width,1,0.2],
+                            [1,wall_width,0.2],
+                            [1,wall_width,0.2]])
+
+
 
     def get_camera_image(self, sz):
         width, height, rgbImg, depthImg, segImg = p.getCameraImage(
@@ -121,48 +143,37 @@ class SimpleDrivingEnv(gym.Env):
                         baseCollisionShapeIndex=colBoxId,
                         basePosition=self.pos_obstacles[i,:])
 
-
     def step(self, action, max_step_multiplier = 1, agent = "alice"):
         # Feed action to the car and get observation of car's state
-        # tic = time.time()
         self.car.apply_action(action)
         p.stepSimulation()
         car_ob = self.car.get_observation()
 
-        # Compute reward as L2 change in distance to goal
-        # dist_to_goal = math.sqrt(((car_ob[0] - self.goal[0]) ** 2 +
-        #                           (car_ob[1] - self.goal[1]) ** 2))
         dist_to_goal = np.linalg.norm(car_ob[:2] - self.goal)
-        # reward = max(self.prev_dist_to_goal - dist_to_goal, 0)
         self.prev_dist_to_goal = dist_to_goal
-
         threshold = 1
+
+        # Reward function
+        # Modify the reward function according to your needs
         reward = 0
-        # Done by running off boundaries
         self.steps += 1
-        # if (car_ob[0] >= 10 or car_ob[0] <= -10 or
-        #         car_ob[1] >= 10 or car_ob[1] <= -10):
-        #     self.done = True
-        #     reward = -3
-        # Done by reaching goal
         if dist_to_goal < threshold:
             self.done = True
             reward = 5
         if self.steps > max_step_multiplier * self.max_steps:
             reward = -2
             self.done = True
+        
         ob = np.array(car_ob, dtype=np.float32)
         gridmap = self.get_observation_image(75, agent)
-        # print("Step time: ", time.time() - tic)
-        return gridmap, ob, reward, self.done, dict()
+        return {'segmentation':gridmap, 'car_qpos':ob}, reward, self.done, dict()
+
 
     def seed(self, seed=None):
         self.np_random, seed = gym.utils.seeding.np_random(seed)
         return [seed]
 
     def reset(self, goal, base_position, base_orientation, agent="alice"):
-        #print(agent)
-        # tic = time.time()
         self.steps = 0
         p.resetSimulation(self.client)
         p.setGravity(0, 0, -10)
@@ -171,11 +182,6 @@ class SimpleDrivingEnv(gym.Env):
         Plane(self.client)
         self.car = Car(self.client, base_position, base_orientation)
 
-        # Set the goal to a random target
-        # x = (self.np_random.uniform(5, 9) if self.np_random.randint(2) else
-        #      self.np_random.uniform(-5, -9))
-        # y = (self.np_random.uniform(5, 9) if self.np_random.randint(2) else
-        #      self.np_random.uniform(-5, -9))
         self.goal = deepcopy(goal)
         self.done = False
 
@@ -187,31 +193,22 @@ class SimpleDrivingEnv(gym.Env):
 
         self.prev_dist_to_goal = math.sqrt(((car_ob[0] - self.goal[0]) ** 2 +
                                            (car_ob[1] - self.goal[1]) ** 2))
-        
-        # boxHalfLength = 5
-        # boxHalfWidth = 5
-        # boxHalfHeight = 5
-        # colBoxId = p.createCollisionShape(p.GEOM_BOX,
-        #                           halfExtents=[boxHalfLength, boxHalfWidth, boxHalfHeight])
-
-        # mass = 100
-        # p.createMultiBody(baseMass=mass,
-        #             baseCollisionShapeIndex=colBoxId,
-        #             basePosition=[5, 5, 0.1])
-        # p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 1)
 
         if agent == "alice":
-            #If agent is alice, reset obstacle positions
+            #If agent is alice, reset obstacle positions (since we want bob to run through same obstacles as alice)
             self.reset_obstacle_positions()
 
         self.draw_obstacles()
         gridmap = self.get_observation_image(75, agent)
         # print("Reset time: ", time.time() - tic)
-        return gridmap, np.array(car_ob, dtype=np.float32)
+        return {'segmentation':gridmap, 'car_qpos':np.array(car_ob, dtype=np.float32)}
 
     def reset_obstacle_positions(self):
+        """
+            reset the obstacle positions based on some probability distribution
+            Write your own reset function
+        """
         self.pos_obstacles[0][0:2] = np.random.uniform(-5,5,2)
-
         mult = -1
         for i in range(2):
             mult = -1*mult
@@ -231,32 +228,7 @@ class SimpleDrivingEnv(gym.Env):
             elif r == 1:
                 self.pos_obstacles[i+2][0:2] = mult*(np.random.uniform(0,5,2)+np.array([-10,5]))
             elif r == 2:
-                self.pos_obstacles[i+2][0:2] = mult*(np.random.uniform(0,5,2)+np.array([-10,0]))
-    
-    def render(self, mode='human'):
-        if self.rendered_img is None:
-            self.rendered_img = plt.imshow(np.zeros((100, 100, 4)))
-
-        # Base information
-        car_id, client_id = self.car.get_ids()
-        proj_matrix = p.computeProjectionMatrixFOV(fov=80, aspect=1,
-                                                   nearVal=0.01, farVal=100)
-        pos, ori = [list(l) for l in
-                    p.getBasePositionAndOrientation(car_id, client_id)]
-        pos[2] = 0.2
-
-        # Rotate camera direction
-        rot_mat = np.array(p.getMatrixFromQuaternion(ori)).reshape(3, 3)
-        camera_vec = np.matmul(rot_mat, [1, 0, 0])
-        up_vec = np.matmul(rot_mat, np.array([0, 0, 1]))
-        view_matrix = p.computeViewMatrix(pos, pos + camera_vec, up_vec)
-
-        # Display image
-        frame = p.getCameraImage(100, 100, view_matrix, proj_matrix)[2]
-        frame = np.reshape(frame, (100, 100, 4))
-        self.rendered_img.set_data(frame)
-        plt.draw()
-        plt.pause(.00001)
+                self.pos_obstacles[i+2][0:2] = mult*(np.random.uniform(0,5,2)+np.array([-10,0]))    
 
     def close(self):
         p.disconnect(self.client)
